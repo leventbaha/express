d82cf4f9ede504e31d43fb249d6c4c96dc841af3
Levent Bahadır Boran <eboran@etu.edu.tr>
2022-09-08 13:14:58 +0000

    built express

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,9 +2,26 @@ const express = require('express')
 
 const router = express.Router()
 
+const users = [
+	{ name: 'marie', age: 20 },
+	{ name: 'markus', age: 23 },
+	{ name: 'thomas', age: 24 },
+]
+
 /* GET users listing. */
+// eslint-disable-next-line consistent-return
 router.get('/', (req, res) => {
-	res.send('respond with a resource')
+	if (req.query.name) {
+		// eslint-disable-next-line eqeqeq
+		return res.send(users.find(user => user.name == req.query.name))
+	}
+	res.send(users)
+})
+
+router.get('/:userId', function (req, res, next) {
+	const user = users[req.params.userId]
+	if (user) res.send(user)
+	else res.sendStatus(404)
 })
 
 module.exports = router
